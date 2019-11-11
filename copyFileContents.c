af0731e404397a2af5a0115c187fd2bc0748e6c2
Rahul Gupta <rahul001048@gmail.com>
2019-11-11 17:31:08 +0530

    Just corrected some programming conventions

--- copyFileContents.c
+++ copyFileContents.c
@@ -13,77 +13,65 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int main(int argc, char** argv) {
-
+int main(int argc, char** argv)
+{
 	char* endptr;
 	int fd1, fd2, l;
 	size_t s1, s2;
 
 	if (argc < 3) {
 	 	perror("Enter source and destination file names");                     
- 		exit(0);
- 	}
+		exit(0);
+	}
 
-	/*Obtaining buffer size from user, setting default to 1000 B, for wrong I/P*/
+/*Obtaining buffer size from user, setting default to 1000 B, for wrong I/P*/
 	if (argc == 3)
- 	l = 1000;
- else {
- 	l = strtol(argv[3], &endptr, 0);
- 	if(*endptr)
- 	l = 1000;
- 	}
+		l = 1000;
+	else {
+	l = strtol(argv[3], &endptr, 0);
+	if(*endptr)
+	l = 1000;
+	}
  	
  void* buf=malloc(l);
 
 /* Opening source file descriptor */
- fd1 = open(argv[1], O_RDONLY);
+	fd1 = open(argv[1], O_RDONLY);
 
- if(fd1 == -1){
- 	perror("could not open source file");
- 	exit(EXIT_FAILURE);
- 	}
+	if(fd1 == -1){
+		perror("could not open source file");
+		exit(EXIT_FAILURE);
+		}
 
 /* Opening/Creating destination file descriptor */ 
- fd2 = open(argv[2], O_WRONLY|O_CREAT, 0666);
+	fd2 = open(argv[2], O_WRONLY|O_CREAT, 0666);
 
- if(fd2 == -1){
- 	perror("could not create destination file");
- 	exit(EXIT_FAILURE);
- 	}
+	if(fd2 == -1){
+		perror("could not create destination file");
+		exit(EXIT_FAILURE);
+	}
 
 /* Copying source contents to destination in chuncks of buffer size in loop */
- while((s1 = read(fd1, buf, l)) != 0) {
-        if(s1 == -1){
- 		perror("cannot read from source file");
- 		exit(EXIT_FAILURE);
- 		}
-        s2 = write(fd2, buf, l);
- 	if(s2 == -1){
- 		perror("cannot write into destination file");
- 		exit(EXIT_FAILURE);
- 		}
+	while((s1 = read(fd1, buf, l)) != 0) {
+		if(s1 == -1){
+			perror("cannot read from source file");
+			exit(EXIT_FAILURE);
+		}
+		s2 = write(fd2, buf, l);
+		if(s2 == -1){
+			perror("cannot write into destination file");
+			exit(EXIT_FAILURE);
+		}
 	}
 
 /* Closing source and destination files */
- if( close(fd1) == -1){
- 	perror("couldn't close source file");
- 	exit(EXIT_FAILURE);
- 	}
- if( close(fd2) == -1){
- 	perror("couldn't close destination file");
- 	exit(EXIT_FAILURE);
- 	} 
- free(buf);
-}
-3], &endptr, 0);
- 	if(*endptr)
- 	l = 1000;
- 	}
- 	
- void* buf=malloc(l);
-
-/* Opening source file descriptor */
- fd1 = open(argv[1], O_RDONLY);
-
- if(fd1 == -1){
- 
+	if( close(fd1) == -1){
+		perror("couldn't close source file");
+		exit(EXIT_FAILURE);
+		}
+	if( close(fd2) == -1){
+		perror("couldn't close destination file");
+		exit(EXIT_FAILURE);
+	}
+	free(buf);
+} 
