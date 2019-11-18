054bbfca5977c48a576c3daaecf36c25998b7bfe
Rahul Gupta <rahul001048@gmail.com>
2019-11-18 14:36:19 +0530

    Correcting coding conventions

--- readIntgsFromFile.c
+++ readIntgsFromFile.c
@@ -9,22 +9,21 @@
 #include <signal.h>
 #include <errno.h>
 
-
 void readFromFile(char *path)
-{ 	
+{
 	int fd1, s1;
 	int* buf1;
 
 	fd1 = open(path, O_RDONLY);
 	if(fd1 == -1){
-	 	perror("could not open source file");
-	 	exit(EXIT_FAILURE);
-	 	}
+		perror("could not open source file");
+		exit(EXIT_FAILURE);
+	}
 	if((buf1 = (int*)malloc(sizeof(int))) == NULL) {
 		perror("cannot allocate memory");
 		if(close(fd1) == -1)
 	 		perror("couldn't close source file");
-	 	_exit(EXIT_FAILURE);
+		_exit(EXIT_FAILURE);
 		}
 	
 	while((s1 = read(fd1, buf1, sizeof(int))) != 0) {
@@ -32,15 +31,14 @@ void readFromFile(char *path)
 				perror("cannot read from source file");
 				free(buf1);
 				if(close(fd1) == -1)
-	 				perror("couldn't close source file"); 	
+					perror("couldn't close source file");
 				exit(EXIT_FAILURE);
-				}
+			}
 			printf("%d ",*buf1);
-		}
+	}
 
 	free(buf1);
 	if(close(fd1) == -1)
 		perror("couldn't close source file"); 	
 				
 }
-
